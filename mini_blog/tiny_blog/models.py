8e76f804237db615cff8f37e04fb10ce102f8b40
Jordan Williams <jordanwilliams288@gmail.com>
2018-11-19 17:42:40 -0600

    models and beginning urls

--- mini_blog/tiny_blog/models.py
+++ mini_blog/tiny_blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=500, blank=True)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=500)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+
+class Blog(models.Model):
+    header = models.CharField(max_length=100)
+    post_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
